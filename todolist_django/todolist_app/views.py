74851c6d7b24315ecaff8dfd0e5e9a1be5f81f00
liu-jinxii <ryanlucan2024@outlook.com>
2025-02-04 22:56:07 +0800

    databases of django

--- todolist_django/todolist_app/views.py
+++ todolist_django/todolist_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def todo_list(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos_list.html", {"todos": items})
